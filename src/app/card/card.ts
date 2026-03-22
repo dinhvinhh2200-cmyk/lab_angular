a897e130e6de2c528a2ece9914b5f99249025985
nguyen vinh <dinhvinhh2200@gmail.com>
2026-03-22 22:06:34 +0700

    fix xong phần xử lý số của hình

--- src/app/card/card.ts
+++ src/app/card/card.ts
@@ -14,6 +14,33 @@ export class Card {
   card_mota = input.required<string>();
   discount = input<number>();
 
+  // Chỉ số ảnh hiện tại
+  currentIndex = signal(0);
+
+  next() {
+    const totalImg = this.listImg()?.length || 0;
+    const current = this.currentIndex();
+
+    // Nếu đang ở ảnh cuối cùng (vị trí bằng tổng số ảnh trừ 1)
+    if (current === totalImg - 1) {
+      this.currentIndex.set(0); // Quay về ảnh đầu tiên
+    } else {
+      this.currentIndex.set(current + 1); // Nếu không thì tăng thêm 1
+    }
+  }
+
+  prev() {
+    const totalImg = this.listImg()?.length || 0;
+    const current = this.currentIndex();
+
+    // Nếu đang ở ảnh đầu tiên
+    if (current === 0) {
+      this.currentIndex.set(totalImg - 1); // Nhảy đến ảnh cuối cùng
+    } else {
+      this.currentIndex.set(current - 1); // Nếu không thì lùi lại 1
+    }
+  }
+
   // xử lý tăng giảm quantity
   quantily = signal(1);
   increase() {
@@ -52,7 +79,7 @@ export class Card {
 
   // xử lý nút yêu thích
   isFavorite = signal(false);
-  onFavoriteChange = output<{title: string, isAdd: boolean}>()
+  onFavoriteChange = output<{ title: string; isAdd: boolean }>();
   toggleFavorite() {
     this.isFavorite.update((initValue) => !initValue);
     // this.isFavorite()
@@ -60,13 +87,13 @@ export class Card {
     //   : alert(`Bạn đã bỏ yêu thích sản phẩm ${this.card_title()}`);
     this.onFavoriteChange.emit({
       title: this.card_title(),
-      isAdd: this.isFavorite()
-    })
+      isAdd: this.isFavorite(),
+    });
   }
 
   // xử lý nút buy now
-  onBuy = output<string>()
-  handleBuyNow () {
-    this.onBuy.emit(this.card_title())
+  onBuy = output<string>();
+  handleBuyNow() {
+    this.onBuy.emit(this.card_title());
   }
 }
